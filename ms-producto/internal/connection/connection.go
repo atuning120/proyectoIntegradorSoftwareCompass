680a829fd38afc53a8d31e35077adaf6ccfdea50
elfaldia <diego.17rojas2002@gmail.com>
2024-11-11 18:30:01 -0300

    a

--- ms-producto/internal/connection/connection.go
+++ ms-producto/internal/connection/connection.go
@@ -1,12 +1,12 @@
 package connection
 
 import (
-    "context"
-    "fmt"
+	"context"
+	"fmt"
 
-    "go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var clientInstance *mongo.Client
@@ -15,30 +15,30 @@ var mongoOnce = false
 
 // Conectar a la base de datos MongoDB
 func ConnectToMongoDB() (*mongo.Client, error) {
-    // Usa el patrón Singleton para evitar múltiples conexiones
-    if clientInstance == nil && !mongoOnce {
-        serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	// Usa el patrón Singleton para evitar múltiples conexiones
+	if clientInstance == nil && !mongoOnce {
+		serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 
-        opts := options.Client().
-            ApplyURI("mongodb://localhost:27017"). // Cambia la URI a la instancia local
-            SetServerAPIOptions(serverAPI)
+		opts := options.Client().
+			ApplyURI("mongodb://localhost:27017").
+			SetServerAPIOptions(serverAPI)
 
-        client, err := mongo.Connect(context.TODO(), opts)
-        if err != nil {
-            clientInstanceError = fmt.Errorf("error al conectar con MongoDB: %v", err)
-            return nil, clientInstanceError
-        }
+		client, err := mongo.Connect(context.TODO(), opts)
+		if err != nil {
+			clientInstanceError = fmt.Errorf("error al conectar con MongoDB: %v", err)
+			return nil, clientInstanceError
+		}
 
-        // Enviar un ping para confirmar la conexión
-        err = client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err()
-        if err != nil {
-            clientInstanceError = fmt.Errorf("error al hacer ping a MongoDB: %v", err)
-            return nil, clientInstanceError
-        }
+		// Enviar un ping para confirmar la conexión
+		err = client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err()
+		if err != nil {
+			clientInstanceError = fmt.Errorf("error al hacer ping a MongoDB: %v", err)
+			return nil, clientInstanceError
+		}
 
-        clientInstance = client
-        mongoOnce = true
-        fmt.Println("Ping exitoso. ¡Conectado a MongoDB!")
-    }
-    return clientInstance, clientInstanceError
+		clientInstance = client
+		mongoOnce = true
+		fmt.Println("Ping exitoso. ¡Conectado a MongoDB!")
+	}
+	return clientInstance, clientInstanceError
 }
